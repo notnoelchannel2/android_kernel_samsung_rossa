a9ed9b267ec0255e960505bef7016450a1ed17fc
Jordan Crouse <jcrouse@codeaurora.org>
2014-12-15 10:36:59 -0700

    msm: kgsl: Correctly handle page allocation failure
    If we fail on the first page allocation iteration in
    _kgsl_sharedmem_page_alloc then sglen will be 0. Do not blindly
    set sg_mark_end() on sg[sglen - 1] without verifying that we
    allocated at least one block. We can also skip iterating the
    sglist when freeing if sglen is 0.
    
    CRs-Fixed: 771199
    Change-Id: Ic0dedbad22c88c1f9efccffddaf266a9a9af4b08
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -699,7 +699,9 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 			 */
 			memdesc->sglen = sglen;
 			memdesc->size = (size - len);
-			sg_mark_end(&memdesc->sg[sglen - 1]);
+
+			if (sglen > 0)
+				sg_mark_end(&memdesc->sg[sglen - 1]);
 
 			if (sglen > 0)
 				sg_mark_end(&memdesc->sg[sglen - 1]);
